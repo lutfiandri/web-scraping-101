38b893e5b236892839bc24bc64094ca07b3fb73d
lutfiandri <lutfiandri232@gmail.com>
2025-08-13 10:06:29 +0700

    feat: B6

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,21 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://web-scraping-101.lutfiandri.my.id",
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "Web Scraping 101",
       // components: ...
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/lutfiandri/web-scraping-101",
+        },
+        {
+          icon: "rocket",
+          label: "Lutfi Andriyanto Personal Website",
+          href: "https://www.lutfiandri.my.id",
         },
       ],
       sidebar: [
